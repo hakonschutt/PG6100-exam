d33b58ca546b6d647e23370fa548b8900681403a
Kleppa <jarandwaagekleppa@gmail.com>
2018-10-14 19:17:55 +0200

    #setup : fixed template runner

--- backend/template/src/main/kotlin/org/bjh/TemplateRunner.kt
+++ backend/template/src/main/kotlin/org/bjh/TemplateRunner.kt
@@ -1,17 +1,9 @@
+package main.kotlin.org.bjh
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-class TemplateRunner {
-
-
-
-
-    companion object {
-
-        @JvmStatic
-        fun main(args: Array<String>) {
-            SpringApplication.run(TemplateRunner::class.java, *args)
-        }
-    }
+class TemplateRunner
+ fun main(args: Array<String>) {
+    SpringApplication.run(TemplateRunner::class.java, *args)
 }
