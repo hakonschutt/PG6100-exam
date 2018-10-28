6f177c8952028b26751f474e11e45ae12c15e2fe
Håkon Wang Schutt <hakon@schutt.no>
2018-10-28 17:03:12 +0100

    finished footer component

--- frontend/src/components/layouts/Footer.js
+++ frontend/src/components/layouts/Footer.js
@@ -1,9 +1,38 @@
 import React from 'react';
 
+import Logo from '../../assets/imgs/logo_white.svg';
+
 const Footer = () => {
 	return (
 		<footer>
-			<h1>Footer</h1>
+			<div className="wrap">
+				<div className="footer-top">
+					<div>
+						<div className="img-wrap">
+							<img src={Logo} alt="house of movies logo" />
+						</div>
+					</div>
+					<div>
+						<h3>Customer service</h3>
+						<ul>
+							<li>Frequently asked questions</li>
+							<li>Rental</li>
+							<li>Sign up for movie news</li>
+						</ul>
+					</div>
+					<div>
+						<h3>Policies</h3>
+						<ul>
+							<li>Privacy policy</li>
+							<li>Cookie policy</li>
+							<li>Member policy</li>
+						</ul>
+					</div>
+				</div>
+				<div className="footer-bottom">
+					<span>©{new Date().getFullYear()} House of movies</span>
+				</div>
+			</div>
 		</footer>
 	);
 };
