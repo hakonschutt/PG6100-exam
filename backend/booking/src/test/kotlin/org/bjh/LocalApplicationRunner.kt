92fd4785b783bb4826cb8c1c7465fd54189971ba
Devops user <schhak16@student.kristiania.no>
2018-12-07 20:15:57 +0100

    added prep data to tests

--- backend/booking/src/test/kotlin/org/bjh/LocalApplicationRunner.kt
+++ backend/booking/src/test/kotlin/org/bjh/LocalApplicationRunner.kt
@@ -1,6 +1,8 @@
 package org.bjh
 
 import io.restassured.RestAssured
+import org.bjh.entity.BookingEntity
+import org.bjh.entity.TicketEntity
 import org.bjh.repository.BookingRepository
 import org.bjh.repository.TicketRepository
 import org.junit.After
@@ -10,6 +12,10 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.web.server.LocalServerPort
 import org.springframework.test.context.junit4.SpringRunner
+import java.util.Random
+import kotlin.collections.ArrayList
+
+
 
 /**
  * @author hakonschutt
@@ -18,6 +24,8 @@ import org.springframework.test.context.junit4.SpringRunner
 @SpringBootTest(classes = [(BookingApplicationRunner::class)],
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 abstract class LocalApplicationRunner {
+
+    private val bookingEntities = ArrayList<BookingEntity>()
 
     @Autowired
     protected lateinit var bookingRepository : BookingRepository
@@ -28,6 +36,40 @@ abstract class LocalApplicationRunner {
     @LocalServerPort
     protected var port = 0
 
+    fun prepTestData() {
+        val userList = listOf(1L, 2L, 3L, 4L)
+        val eventList = listOf(5L, 6L, 7L, 8L)
+        val rand = Random()
+
+        bookingEntities.removeAll(bookingEntities)
+
+        listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 ,13 ,14 ,15)
+            .stream()
+            .forEach { data ->
+                bookingEntities.add(
+                    BookingEntity(
+                        id = null,
+                        user = userList[rand.nextInt(userList.size)],
+                        event = eventList[rand.nextInt(eventList.size)],
+                        tickets = setOf(
+                            TicketEntity(
+                                id = null,
+                                seat = "seat-$data",
+                                price = 10.00
+                            )
+                        )
+                    )
+                )
+            }
+
+        bookingRepository.run {
+            deleteAll()
+            bookingEntities.forEach {
+                save(it)
+            }
+        }
+    }
+
     @Before
     @After
     fun clean() {
@@ -35,5 +77,7 @@ abstract class LocalApplicationRunner {
         RestAssured.port = port
         RestAssured.basePath = "/api/bookings"
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails()
+
+        prepTestData()
     }
 }
