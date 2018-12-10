8f870d7fa7ace5aa8a04a5285a2d3329b12b3a36
kleppa <jarandwaagekleppa@gmail.com>
2018-12-10 22:40:02 +0100

    Working on genric http code

--- backend/events/src/main/kotlin/org/bjh/component/HttpService.kt
+++ backend/events/src/main/kotlin/org/bjh/component/HttpService.kt
@@ -1,7 +1,21 @@
 package org.bjh.component
 
+import org.bjh.dto.VenueDto
 import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.getForObject
+import kotlin.reflect.KClass
 
 @Component
 class HttpService {
+    fun <T : VenueDto, MovieDto, RoomDto> getReq(url: String, clazz: KClass<T>): List<T> {
+
+        val template = RestTemplate()
+
+        val data = template.getForObject(url, clazz::class.java)
+
+        return emptyList()
+
+//https://spring.io/guides/gs/consuming-rest/
+    }
 }
--- backend/events/src/main/kotlin/org/bjh/resolver/EventResolver.kt
+++ backend/events/src/main/kotlin/org/bjh/resolver/EventResolver.kt
@@ -1,25 +1,31 @@
 package org.bjh.resolver
 
 import com.coxautodev.graphql.tools.GraphQLResolver
+import org.bjh.component.HttpService
 import org.bjh.type.EventType
 import org.bjh.type.MovieType
 import org.bjh.type.RoomType
 import org.bjh.type.VenueType
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import org.apache.http.client.methods.HttpGet;
 
 @Component
 class EventResolver : GraphQLResolver<EventType> {
-    fun movie(event : EventType): MovieType? {
+    @Autowired
+    private lateinit var httpService: HttpService
+
+    fun movie(event: EventType): MovieType? {
         // TODO: Create HTTP request
         return null
     }
 
-    fun venue(event : EventType): VenueType? {
+    fun venue(event: EventType): VenueType? {
         // TODO: Create HTTP request
         return null
     }
 
-    fun room(event : EventType): RoomType? {
+    fun room(event: EventType): RoomType? {
         // TODO: Create HTTP request
         return null
     }
