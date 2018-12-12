3fe103792e8ae6a7dd59d2bbeb15c04058533de7
hakonschutt <hakon@schutt.no>
2018-12-12 18:27:41 +0100

    added eureka to booking servic

--- backend/booking/src/main/kotlin/org/bjh/BookingApplicationRunner.kt
+++ backend/booking/src/main/kotlin/org/bjh/BookingApplicationRunner.kt
@@ -2,6 +2,7 @@ package org.bjh
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 import org.springframework.context.annotation.Bean
 import springfox.documentation.builders.ApiInfoBuilder
 import springfox.documentation.builders.PathSelectors
@@ -15,7 +16,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2
  */
 @SpringBootApplication
 @EnableSwagger2
-
+@EnableEurekaClient
 class BookingApplicationRunner {
 
     @Bean
