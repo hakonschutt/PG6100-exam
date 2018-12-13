c7581c0536dfa11c0a59eb74b649c0a91e75df8b
hakonschutt <hakon@schutt.no>
2018-12-13 15:35:15 +0100

    got booking instance on eureka

--- backend/eureka/src/main/kotlin/org/bjh/EurekaApplicationRunner.kt
+++ backend/eureka/src/main/kotlin/org/bjh/EurekaApplicationRunner.kt
@@ -3,10 +3,10 @@ package org.bjh
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
-@EnableEurekaServer
-@SpringBootApplication
-class EurekaApplicationRunner{}
 
+@SpringBootApplication
+@EnableEurekaServer
+class EurekaApplicationRunner
 
 fun main(args: Array<String>) {
     SpringApplication.run(EurekaApplicationRunner::class.java, *args)
--- backend/gateway/src/main/kotlin/org/bjh/GatewayApplicationRunner.kt
+++ backend/gateway/src/main/kotlin/org/bjh/GatewayApplicationRunner.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 class GatewayApplication
 fun main(args: Array<String>) {
     SpringApplication.run(GatewayApplication::class.java, *args)
