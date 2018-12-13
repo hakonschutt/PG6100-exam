7ed03c42c2d52157d60db34ef8e097676bdecb70
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-12-13 22:00:56 +0100

    On the way of fixing pom and eureka issues

--- backend/gateway/src/main/kotlin/org/bjh/GatewayApplicationRunner.kt
+++ backend/gateway/src/main/kotlin/org/bjh/GatewayApplicationRunner.kt
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-@SpringBootApplication
 @EnableDiscoveryClient
-class GatewayApplication
+@SpringBootApplication
+class GatewayApplication{
+}
+
 fun main(args: Array<String>) {
     SpringApplication.run(GatewayApplication::class.java, *args)
 }
