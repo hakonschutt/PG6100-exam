005675a7f7528f3d585a9fdb810dd6dde515a6ff
hakonschutt <hakon@schutt.no>
2018-12-13 22:37:03 +0100

    finished not found pag

--- frontend/src/pages/NotFoundPage.js
+++ frontend/src/pages/NotFoundPage.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const NotFoundPage = () => {
+const NotFound = () => {
 	return (
-		<div className="not-found">
-			<div className="not-found-inner">
-				<h1>404</h1>
-				<div className="text-info">
-					<span>Page not found.</span>
-					<Link to="/">
-						<span className="text">Back to frontpage</span>
-					</Link>
+		<div
+			className="page-wrap d-flex flex-row align-items-center"
+			style={{ minHeight: 'calc(100vh - 400px)' }}
+		>
+			<div className="container">
+				<div className="row justify-content-center">
+					<div className="col-md-12 text-center">
+						<span className="display-1 d-block">404</span>
+						<div className="mb-4 lead">
+							{'We can\'t seem to find the page you\'re looking for.'}
+						</div>
+						<Link to="/" className="btn btn-link">
+							Back to Home
+						</Link>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
 };
 
-export default NotFoundPage;
+export default NotFound;
