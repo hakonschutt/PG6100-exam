10299220c490edf8ea7c157fdf33a4477d4cffad
bjornosal <bjorn.olav.salvesen@gmail.com>
2018-12-13 20:45:46 +0100

    Does any of this work?

--- backend/eureka/src/main/kotlin/org/bjh/EurekaApplicationRunner.kt
+++ backend/eureka/src/main/kotlin/org/bjh/EurekaApplicationRunner.kt
@@ -3,13 +3,6 @@ package org.bjh
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
-<<<<<<< HEAD
-
-@SpringBootApplication
-@EnableEurekaServer
-class EurekaApplicationRunner
-=======
->>>>>>> 9ef48d48059027e7ed4467b0a6ff940fb00d0dd9
 
 @SpringBootApplication
 @EnableEurekaServer
